28e9fea664d98166a04ba2f373a56a19b779afb2
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-09-14 21:56:22 -0500

    guardando en estados el nombre de usuario y numero de dni

--- src/containers/IntroductionContainer.jsx
+++ src/containers/IntroductionContainer.jsx
@@ -4,9 +4,7 @@ import Introduction from '../components/Introduction';
 import { SubmissionError } from "redux-form";
 class IntroductionContainer extends React.Component {
     handleSubmitIntroduction = values => {
-        
         const { dni, name } = values;
-        console.log(dni, name);
         if(dni === undefined && name === undefined){
             throw new SubmissionError({ dni: "Este campo es requerido", name: "Este campo es requerido"  });
         }else if(dni === undefined) {
@@ -14,7 +12,7 @@ class IntroductionContainer extends React.Component {
         } else if ( name === undefined) {
             throw new SubmissionError({ name: "Este campo es requerido" });
         } else {
-            console.log('todo bien');
+            this.props.updateNameAndNumberDni(name, dni);
         }
       };
     render() {
--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -3,8 +3,23 @@ import IntroductionContainer from './IntroductionContainer';
 import { connect } from 'react-redux';
 
 class Index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          name: '',
+          numberDni: '',
+        };
+      }
+
+    updateNameAndNumberDni = (newName, newNumberDni) => {
+        this.setState({
+            name: newName,
+            numberDni: newNumberDni,
+        }, () => console.log(this.state.name, this.state.numberDni))
+    }
+    
     render() {
-        return (<IntroductionContainer/>)
+        return (<IntroductionContainer updateNameAndNumberDni={this.updateNameAndNumberDni} />)
     }
 }
 
